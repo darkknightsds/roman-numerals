596c798d73dbb10ad2c24ae1be36898507d493a7
Sara Schultz and Shane Stafford <shane@>
2017-02-23 18:50:44 -0800

    add functionality for hundreds and thousands columns

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,33 @@ var numberToConvert = ""
 var output = ""
 var ones = ""
 var tens = ""
+var hundreds = ""
 var invalid = "Try again."
 var onesColumn = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
 var tensColumn = ["X", "XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+var hundredsColumn = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+var thousandsColumn = ["M", "MM", "MMM"]
 var convertedNumber = function(input) {
   if (input.length === 1) {
     var ones = input.charAt(0);
-    return ones = onesColumn[input - 1];
+    return onesColumn[ones - 1];
   } else if (input.length === 2) {
     var tens = input.charAt(0);
     var ones = input.charAt(1);
     return tensColumn[tens - 1] + onesColumn[ones - 1];
   } else if (input.length === 3) {
-    return input = "0" + input;
+    var hundreds = input.charAt(0);
+    var tens = input.charAt(1);
+    var ones = input.charAt(2);
+    return hundredsColumn[hundreds - 1] + tensColumn[tens - 1] + onesColumn[ones - 1];
+  } else if (input.length === 4) {
+    var thousands = input.charAt(0);
+    var hundreds = input.charAt(1);
+    var tens = input.charAt(2);
+    var ones = input.charAt(3);
+    return thousandsColumn[thousands - 1] + hundredsColumn[hundreds - 1] + tensColumn[tens - 1] + onesColumn[ones - 1];
   } else {
-    return input;
+    return invalid;
   }
   // for (var index = 1; index = 1; index += 4) {
   //   return onesColumn[input - 1];
